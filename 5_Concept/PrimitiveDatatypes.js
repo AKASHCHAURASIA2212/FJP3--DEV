f18202409a7f060c2d2d36faa34b0628217a2b22
AKASHCHAURASIA2212 <AKASHCHAURASIA2212@gmail.com>
2022-11-23 23:11:39 +0530

    commit

---
+++ 5_Concept/PrimitiveDatatypes.js
@@ -0,0 +1,32 @@
+// let a = 'Hello'
+
+// console.log(a , typeof(a))
+
+// let b = 2.5
+
+// console.log(b , typeof(b))
+
+// let c = true
+// console.log(c , typeof(c))
+
+
+// let d = undefined
+// console.log(d , typeof(d))
+
+// let e = null
+// console.log(e , typeof(e))
+
+
+
+let firstPerson = 'surya'
+
+let secondPerson = firstPerson
+
+firstPerson = 'ravi'
+
+console.log(firstPerson) // ravi
+console.log(secondPerson) // surya
+
+
+
+
---
+++ 5_Concept/RefDatatype.js
@@ -0,0 +1,34 @@
+// let arr = [1 , 3 , 3]
+
+// console.log(arr , typeof(arr))
+
+
+// let obj = {
+//        name : 'Adam',
+//        age :23
+// }
+
+// console.log(obj , typeof(obj))
+
+
+
+// function greet(){
+//        console.log('Hello')
+// }
+
+// console.log(greet , typeof(greet))
+
+let firstPerson = {
+    name: 'akash',
+    age: 22
+}
+
+let secondPerson = firstPerson
+
+secondPerson.name = 'nuet'
+
+console.log(firstPerson)
+console.log(secondPerson)
+
+
+
